ad53f872f021757d241aa71f6518278ac52c8f70
khakhkharBhumika <khakhkharbhumika624@gmail.com>
2024-09-12 20:01:15 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+   
+    <form action="dynamic.php"method="GET" >
+        Enter About Me<input type="text"Name="about"><br>
+        Enter Description<input type="text"Name="php"><br>
+        Enter Department<input type="text"Name="MCA"><br>
+        Enter Assignments<input type="text"Name="Task1"><br>
+       
+        <input type="submit">
+
+
+    </form>
+</html>
